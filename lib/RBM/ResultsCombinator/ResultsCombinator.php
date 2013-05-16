e7110af43f8bab54ef833e67daf1c9e88330991e
romainbessuges <romainbessuges@gmail.com>
2013-05-16 10:08:55 +0200

    Massive refactoring

--- lib/RBM/ResultsCombinator/ResultsCombinator.php
+++ lib/RBM/ResultsCombinator/ResultsCombinator.php
@@ -4,8 +4,242 @@ namespace RBM\ResultsCombinator;
 
 class ResultsCombinator
 {
-    const DEFAULT_SEPARATOR = ".";
+    const DEFAULT_SEPARATOR = '.';
 
+    protected $_identifier;
+
+    protected $_separator = self::DEFAULT_SEPARATOR;
+
+    protected $_groups = [];
+
+    protected $_class = '\stdClass';
+
+    protected $_subClasses = [];
+
+    protected $_methods = [];
+
+    /**
+     * @param $class
+     */
+    public function setClass($class)
+    {
+        $this->_class = $class;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClass()
+    {
+        return $this->_class;
+    }
+
+    /**
+     * @param $property
+     * @param $class
+     */
+    public function addSubClass($property, $class)
+    {
+        $this->_subClasses[$property] = $class;
+    }
+
+    /**
+     * @param $object
+     * @param $methodName
+     * @param $args
+     * @param string $scope
+     */
+    public function addMethod($object, $methodName, $args)
+    {
+        if (!isset($this->_methods[$object])) {
+            $this->_methods[$object] = [];
+        }
+        $this->_methods[$object][$methodName] = [
+            "args" => $args,
+            "exec" => [],
+        ];
+    }
+
+    public function setIdentifier($identifier)
+    {
+        $this->_identifier = $identifier;
+    }
+
+    public function getIdentifier()
+    {
+        return $this->_identifier;
+    }
+
+    public function setSeparator($separator)
+    {
+        $this->_separator = $separator;
+    }
+
+    public function getSeparator()
+    {
+        return $this->_separator;
+    }
+
+    public function addGroup($path, $identifier, $class = '\stdClass')
+    {
+
+        if (isset($this->_groups[$path])) {
+            throw new \InvalidArgumentException("A group for path $path already exists");
+        }
+
+        $this->_groups[$path] = [
+            "identifier" => $identifier,
+            "class"      => $class,
+        ];
+    }
+
+    public function process($rows)
+    {
+        $data   = [];
+        $flat   = [];
+        $sepLen = strlen($this->_separator);
+
+        foreach ($rows as $row) {
+
+            $identifierValue = $row[$this->_identifier];
+
+            foreach ($row as $key => $value) {
+
+                $buffer = "";
+                $path   = "";
+
+                if (!isset($data[$identifierValue])) {
+                    $data[$identifierValue] = new $this->_class();
+                    $flat[$identifierValue] = [];
+                }
+
+                $parent     = & $data[$identifierValue];
+                $flatParent = & $flat[$identifierValue];
+
+                // store the key length for iteration optim
+                $keyLen = strlen($key);
+                // iterate on chars
+                $erase = false;
+
+                $skip = false;
+                for ($i = 0; $i < $keyLen; $i++) {
+                    // if we find a separator
+                    if (substr($buffer, -$sepLen) === $this->_separator) {
+                        // remove the separator from the buffer
+                        $buffer = substr($buffer, 0, -$sepLen);
+                        // and concat to whole path
+                        $path .= $buffer;
+                        // if we find a group
+                        if (isset($this->_groups[$path])) {
+                            // we look up for the grouping column
+
+                            // if the holder for group does not exist, create an array
+                            if (!isset($parent->$buffer)) {
+                                $parent->$buffer = [];
+                            }
+
+                            $groupKey = $path . $this->_separator . $this->_groups[$path]["identifier"];
+                            // and fetch the value
+                            $groupVal = $row[$groupKey];
+                            // if the the value's not empty
+                            if ($groupVal) {
+                                // if the group isn't already created
+                                $group     = & $parent->$buffer;
+                                $flatGroup = & $flatParent[$buffer];
+                                if (!isset($group[$groupVal])) {
+                                    // get the class
+                                    $groupClass = $this->_groups[$path]["class"];
+                                    // and create
+                                    $group[$groupVal]     = new $groupClass();
+                                    $flatGroup[$groupVal] = [];
+                                }
+                                // make the parent sur group class
+                                $parent     = & $group[$groupVal];
+                                $flatParent = & $flatGroup[$groupVal];
+                            } else {
+                                $skip = true;
+                            }
+                        } else {
+                            // subclass
+                            if (!isset($parent->$buffer)) {
+                                $cls                 = (isset($this->_subClasses[$path])) ? $this->_subClasses[$path] : '\stdClass';
+                                $parent->$buffer     = new $cls();
+                                $flatParent[$buffer] = [];
+                            }
+
+                            if (isset($this->_methods[$path])) {
+                                $method = & $this->_methods[$path];
+                                foreach ($method as $name => $detail) {
+                                    $method[$name]["exec"][] = [
+                                        "target"  => & $parent->$buffer,
+                                        "context" => & $flat[$identifierValue],
+                                    ];
+                                }
+                            }
+                            $parent     = & $parent->$buffer;
+                            $flatParent = & $flatParent[$buffer];
+                        }
+
+                        $path .= $this->_separator;
+                        $buffer = "";
+                    }
+                    $buffer .= $key[$i];
+                }
+
+                if (!$skip && (is_a($parent, '\stdClass') || property_exists(get_class($parent), $buffer))) {
+                    $parent->$buffer = $value;
+                }
+
+                $flatParent[$buffer] = $value;
+            }
+        }
+
+        foreach ($this->_methods as $method) {
+            foreach ($method as $methodName => $methodDetail) {
+                foreach ($methodDetail["exec"] as $exec) {
+                    $target  = $exec["target"];
+                    $context = $exec["context"];
+                    $params  = $this->_getParams($context, $methodDetail['args']);
+                    call_user_func_array([$target, $methodName], $params);
+                }
+            }
+        }
+
+        return $data;
+    }
+
+    protected function _getParams($context, $args)
+    {
+        $values = [];
+        $sepLen = strlen($this->getSeparator());
+        foreach($args as $arg){
+            $keyLen = strlen($arg);
+            $buffer = "";
+            $parent = & $context;
+
+            for ($i = 0; $i < $keyLen; $i++) {
+                // if we find a separator
+                if (substr($buffer, -$sepLen) === $this->getSeparator()) {
+                    // remove the separator from the buffer
+                    $buffer = substr($buffer, 0, -$sepLen);
+                    $parent = & $parent[$buffer];
+                    $buffer = "";
+                }
+                $buffer .= $arg[$i];
+            }
+            $values[] = $parent[$buffer];
+        }
+        return $values;
+    }
+
+    /**
+     * @deprecated
+     * @param $rows
+     * @param $identifier
+     * @param array $groups
+     * @param string $separator
+     * @return array
+     */
     public function combine($rows, $identifier, $groups = [], $separator = self::DEFAULT_SEPARATOR)
     {
         $data   = [];
@@ -62,4 +296,5 @@ class ResultsCombinator
 
         return $data;
     }
+
 }
--- tests/test.php
+++ tests/test.php
@@ -29,30 +29,120 @@ function loadCSV($filename)
     }
     return $rows;
 }
+/*
+ * "order.id",
+"project.ati_amount",
+"project.vat_amount",
+"project.et_amount",
+"client.id",
+"client.company_name",
+"client.email",
+"client.address.line1",
+"client.address.line2",
+"client.address.line3",
+"client.address.postcode",
+"client.address.city",
+"client.contact_name",
+"client.contact_forename",
+"order.reference",
+"order.ati_amount",
+"order.vat_amount",
+"order.et_amount",
+"product.name",
+"product.subfamily",
+"product.family",
+"pboxer.company_name",
+"pboxer.email",
+"pboxer.address.line1",
+"pboxer.address.line2",
+"pboxer.address.line3",
+"pboxer.address.postcode",
+"pboxer.address.city",
+"pboxer.contact_name",
+"pboxer.contact_forename",
+"order.fees.id",
+"order.fees.quantity",
+"order.fees.unit_amount",
+"order.fees.ati_amount",
+"order.fees.et_amount",
+"order.fees.vat_amount",
+"order.fees.type",
+"product.options.id",
+"product.options.label",
+"product.options.value",
+"product.options.unit"
+ */
+
+class Project {
+    public $ati_amount;
+    public $vat_amount;
+    public $et_amount;
+}
+
+class Order {
+    public $id;
+    public $reference;
+    public $ati_amount;
+    public $vat_amount;
+    public $et_amount;
+    /** @var Fee[] */
+    public $fees = [];
+    public $client;
+}
+
+class Client {
+    public $id;
+    public $company_name;
+    public $email;
+    public $address;
+    public $contact_name;
+    public $contact_forename;
+}
+
+class Fee {
+    public $id;
+    public $quantity;
+    public $unit_amount;
+    public $type;
+    public $ati_amount;
+    public $vat_amount;
+    public $et_amount;
+}
+
+class Option {
+    public $id;
+    public $label;
+    public $value;
+    public $unit;
+}
+
+class Address {
+    public $line1;
+    public $line2;
+    public $line3;
+    protected $_postcode;
+    protected $_city;
+
+    public function setLocation($city, $postcode){
+        $this->_city = $city;
+        $this->_postcode = $postcode;
+    }
+}
 
 $rows = loadCSV(__DIR__ . '/dataset.csv');
 
 $combinator = new \RBM\ResultsCombinator\ResultsCombinator();
+$combinator->setIdentifier("id");
+$combinator->setClass('Order');
+$combinator->addSubClass("project", "Project");
+$combinator->addSubClass("pboxer", "Client");
+$combinator->addSubClass("pboxer.address", "Address");
+$combinator->addSubClass("client", "Client");
+$combinator->addSubClass("client.address", "Address");
+$combinator->addMethod("client.address", "setLocation", ["client.address.city", "client.address.postcode"]);
+$combinator->addGroup("fees", "id", "Fee");
+$combinator->addGroup("options", "id", "Option");
 
-/*
-$bench = [];
-for ($i = 0; $i < 10; $i++) {
-    $t1      = microtime(true);
-
-  */
-    $results = $combinator->combine(
-        $rows,
-        "order__id", [
-            "order__fees"      => "id",
-            "product__options" => "id",
-        ],
-        "__"
-    );
+$results = $combinator->process($rows);
 
 print_r($results);
-    /*
-    $bench[] = microtime(true) - $t1;
-}
-
-echo array_sum($bench) / 100;
-*/
